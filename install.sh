57bcbc7b3e428b8262be67709ac5ce9a7af03834
Ardhy Aradian <bastardhy@gmail.com>
2023-10-08 03:01:01 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 
 cp downloader.desktop $HOME/.local/share/kservices5/ServiceMenus/
 sudo cp yt-dl /usr/bin/
-sudo chmod +x yt-dl
+sudo chmod +x /usr/bin/yt-dl
 printf "Done: File has been installed"
 
